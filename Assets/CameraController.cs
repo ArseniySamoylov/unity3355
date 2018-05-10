d1896a0ad20e90a78437e6d96fae5d96c08e0220
ArseniySamoylov <38934938+ArseniySamoylov@users.noreply.github.com>
2018-05-10 09:58:05 +0300

    Add files via upload

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // расстояние от камеры до объекта
+    private Vector3 offset;
+    // Объект, за который следит камера
+    // Указываем public для того, чтобы после появилось в редакторе
+    public GameObject target;
+    // Use this for initialization
+
+    void Start () {
+        offset = target.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position - offset;
+	}
+}
